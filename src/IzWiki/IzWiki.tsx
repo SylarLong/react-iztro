aef7fb2a735175670d898b2d2e9987be4ee8174b
lucas <lucasji@foxmail.com>
2025-03-31 16:00:41 +0800

    remove local knowledge base upload function

--- src/IzWiki/IzWiki.tsx
+++ src/IzWiki/IzWiki.tsx
@@ -50,45 +50,45 @@ export const IzWiki = ({
       .then((text) => setMd(text));
   };
 
-  const loadMarkdownByName = (name: string) => {
-    console.log("load markdown of", source);
-    const request = window.indexedDB.open("iztro", 1);
-    request.onsuccess = (event) => {
-      const db = (event.target as IDBOpenDBRequest).result;
-      if (!db.objectStoreNames.contains("markdowns")) {
-        loadDefaultMarkdown();
-        return;
-      }
-      const transaction = db.transaction("markdowns", "readonly");
-      const store = transaction.objectStore("markdowns");
-      const getAllKeysRequest = store.getAllKeys(); // 获取所有的键
-      getAllKeysRequest.onsuccess = () => {
-        const keys = getAllKeysRequest.result as string[];
-        const targetKey = keys.find((key) => key.includes(`${name}.md`));
-        if (targetKey) {
-          const fileRequest = store.get(targetKey);
-          fileRequest.onsuccess = () => {
-            const file = fileRequest.result;
-            if (file) {
-              setMd(file.content);
-            } else {
-              loadDefaultMarkdown();
-            }
-          };
-        } else {
-          loadDefaultMarkdown();
-        }
-      };
-    };
-  };
+  // const loadMarkdownByName = (name: string) => {
+  //   console.log("load markdown of", source);
+  //   const request = window.indexedDB.open("iztro", 1);
+  //   request.onsuccess = (event) => {
+  //     const db = (event.target as IDBOpenDBRequest).result;
+  //     if (!db.objectStoreNames.contains("markdowns")) {
+  //       loadDefaultMarkdown();
+  //       return;
+  //     }
+  //     const transaction = db.transaction("markdowns", "readonly");
+  //     const store = transaction.objectStore("markdowns");
+  //     const getAllKeysRequest = store.getAllKeys(); // 获取所有的键
+  //     getAllKeysRequest.onsuccess = () => {
+  //       const keys = getAllKeysRequest.result as string[];
+  //       const targetKey = keys.find((key) => key.includes(`${name}.md`));
+  //       if (targetKey) {
+  //         const fileRequest = store.get(targetKey);
+  //         fileRequest.onsuccess = () => {
+  //           const file = fileRequest.result;
+  //           if (file) {
+  //             setMd(file.content);
+  //           } else {
+  //             loadDefaultMarkdown();
+  //           }
+  //         };
+  //       } else {
+  //         loadDefaultMarkdown();
+  //       }
+  //     };
+  //   };
+  // };
 
   useEffect(() => {
     if (lazy) {
       if (isPopoverOpen) {
-        loadMarkdownByName(source);
+        loadDefaultMarkdown();
       }
     } else {
-      loadMarkdownByName(source);
+      loadDefaultMarkdown();
     }
   }, [isPopoverOpen, lazy]);
 
